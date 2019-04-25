872281bba8cd65e69296a1e5b2e8b7e12caf7d24
David Gayerie <dagaydevel@free.fr>
2019-04-25 10:49:52 +0200

    Ajoutez des fichiers projet.

---
+++ temperature/ConvertisseurTemperature.cs
@@ -0,0 +1,21 @@
+﻿
+namespace temperature
+{
+    class ConvertisseurFarenheit
+    {
+        private double temperature;
+
+        public ConvertisseurFarenheit(double temperature) => Celsius = temperature;
+
+        public double Celsius
+        {
+            get => temperature;
+            set => temperature = value;
+        }
+
+        public double Farenheit()
+        {
+            return 1.8 * Celsius + 32;
+        }
+    }
+}
---
+++ temperature/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace temperature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez une température en Celsius :");
+            double t = Convert.ToDouble(Console.ReadLine());
+            var convertisseur = new ConvertisseurFarenheit(t);
+            Console.WriteLine("Conversion en farenheit : {0}",  convertisseur.Farenheit());
+        }
+    }
+}
